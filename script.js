435022b42655727fcfc52b14af75bd94236e1928
Brandi Cotton <bdcotton52@gmail.com>
2019-09-13 13:29:51 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Set a click handler function for each column that adds an additional disc.
+//     Take turns! Toggle the color of each successive disc added.
+//     Keep track of what color disc is at each position in the board. You should be able to console.log() debugging output after each move showing the state of the board.
+//     Once a column is full (has 6 discs), don't allow any more discs to be added.
+//     Check whether the last disc added completed a four-in-a-row within the column (vertically).
+//     Check whether the last disc added completed four-in-a-row horizontally.
+//     Check whether the last disc added completed four-in-a-row on either an upward- or downward-sloping diagonal. -->
+    
