375cbb1d76bfd3c884ac4590424dfecd2bc725f8
Brandi Cotton <bdcotton52@gmail.com>
2019-09-17 12:15:04 -0500

    Updated file to include skeleton built during demo

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+let currentPlayer = "red" // "red" "black"
+
+// "red" "black" null
+let board = [
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ]
+]
+
+function addDiskToBoard (color, boardToUpdate, selectedColumn) {
+
+    return updatedBoard
+}
+
+function displayBoardInHTML (boardToDisplay) {
+
+    // nothing to return
+}
+
+function checkForEndingCondition (boardToCheck) {
+    // ending conditions: "red win", "black win", "tie", "" (keep playing)
+    return condition
+}
+
+function showMessage () {
+    // Tell the user if someone has won or there is a tie
+}
+
+function togglePlayer (color) {
+    return newColor
+}
+
+function columnClickHandler (evt) {
+    // figure out what column was clicked
+    let clickedColumn = evt.target.dataset.col
+    if (clickedColumn === undefined) {
+        return
+    }
+
+    board = addDiskToBoard(currentPlayer, board, clickedColumn)
+    displayBoardInHTML(board)
+    let condition = checkForEndingCondition(board)
+    // "red win", "black win", "tie", ""
+    if (condition !== "") {
+        showMessage(condition)
+    } else {
+        currentPlayer = togglePlayer(currentPlayer)
+    }
+}
+
+
 // Set a click handler function for each column that adds an additional disc.
 //     Take turns! Toggle the color of each successive disc added.
 //     Keep track of what color disc is at each position in the board. You should be able to console.log() debugging output after each move showing the state of the board.
