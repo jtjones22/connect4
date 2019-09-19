9d9737bb1fc7f783b90f962de213ef343d25ed91
Chelsea <chewh115@gmail.com>
2019-09-19 13:26:09 -0400

    Adds checkForEndingCondition function

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ let board = [
     [ null, null, null, null, null, null, null ]
 ]
 
+const edgeX = board[0].length - 3;
+const edgeY = board.length - 3;
+let currentPlayerWins
+let horizontalWin 
+let verticalWin 
+let diagonalWinRight
+let diagonalWinLeft
+let tie
+let continuePlaying 
+
+function createDisk(currentPlayer) {
+    if (currentPlayer === 'red') {
+        let diskA = document.createElement('div');
+        diskA.classList.add('diskA');
+    }
+    else if (currentPlayer === 'black') {
+        let diskB = document.createElement('div');
+        diskB.classList.add('diskB');
+    }
+}
+
 function addDiskToBoard (color, boardToUpdate, selectedColumn) {
 
     return updatedBoard
@@ -21,12 +42,110 @@ function displayBoardInHTML (boardToDisplay) {
 }
 
 function checkForEndingCondition (boardToCheck) {
-    // ending conditions: "red win", "black win", "tie", "" (keep playing)
-    return condition
+
+    function horizontalWinCheck(boardToCheck) {
+        for(let y = 0; y < board.length; y++){
+            for(let x = 0; x < edgeX; x++) {
+              let cell = board[y][x];
+              if(cell !== null) {
+                if(cell === board[y][x+1] && cell === board[y][x+2] && cell === board[y][x+3] ) {
+                    console.log('You won!');
+                    horizontalWin = true;
+                    return horizontalWin
+                }
+              }
+            }
+          }
+        return false
+    }
+    console.log(horizontalWinCheck(board));
+    
+    function verticalWinCheck(boardToCheck) {
+        for (let y = 0; y < edgeY; y++) {
+            for (let x = 0; x < board[0].length; x++) {
+                cell = board[y][x];
+                if (cell !== null) {
+                    if (cell === board[y+1][x] && cell === board[y+2][x] && cell === board[y+3][x]) {
+                        console.log('You won vertically!');
+                        verticalWin = true;
+                        return verticalWin
+                    }
+                }
+            }
+        }
+        return false
+    }
+    console.log(verticalWinCheck(board));
+    
+    function diagonalLeftWinCheck(boardToCheck) { 
+        for (let y = 0; y < edgeY; y++) {
+            for (let x = 0; x < edgeX; x++) {
+                cell = board[y][x];
+                if (cell !== null) {
+                    if (cell === board[y+1][x+1] && cell === board[y+2][x+2] && cell === board[y+3][x+3]) {
+                        console.log('You won diagonally to the Left!')
+                        diagonalWinLeft = true;
+                        return diagonalWinLeft
+                    }
+                }
+            }
+        }
+        return false
+    }
+    console.log(diagonalLeftWinCheck(board));
+    
+    function diagonalRightWinCheck(boardToCheck) { 
+        for (let y = 3; y < board.length; y++) {
+            for (let x = 0; x < edgeX; x++) {
+                cell = board[y][x];
+                if (cell !== null) {
+                    if (cell === board[y-1][x+1] && cell === board[y-2][x+2] && cell === board[y-3][x+3]) {
+                        console.log('You won diagonally to the Right!')
+                        diagonalWinRight = true;
+                        return diagonalWinRight
+                    }
+                }
+            }
+        }
+        return false
+    }
+    console.log(diagonalRightWinCheck(board));
+
+    function tieCheck(boardToCheck) {
+        let emptySpace = "unavailable"
+        for (let y = 0; y < board.length; y++) {
+            for (let x = 0; x < edgeX; x++) {
+                cell = board[y][x]
+                if(cell === null){
+                    emptySpace = "available"
+                    tie = false
+                    return false
+                }
+            }
+        }
+        if(emptySpace === "unavailable" && horizontalWin !== true && verticalWin !== true && diagonalWinRight !== true && diagonalWinLeft !== true){
+        tie = true
+        return tie;
+        }
+    }
+    console.log(tieCheck(board));
+    
+    
+    if (tie === true) {
+        return "tie"
+    }
+    else if (horizontalWin === true || verticalWin === true || diagonalWinRight === true || diagonalWinLeft === true) {
+
+        return  currentPlayerWins + " wins"
+    }
+    else {
+        return "continuePlaying"
+    }
 }
+console.log(checkForEndingCondition(board));
 
 function showMessage () {
-    // Tell the user if someone has won or there is a tie
+    // Tell the user if someone has won or there is a tie or keep playing
 }
 
 function togglePlayer (color) {
