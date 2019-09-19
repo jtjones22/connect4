cafcadd370e4b8b3a2f304570b4642fdf0093d42
Brandi Cotton <bdcotton52@gmail.com>
2019-09-19 11:07:46 -0500

    Updated .html .css. and .js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 let currentPlayer = "red" // "red" "black"
 
 // "red" "black" null
-let board = [
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ],
-    [ null, null, null, null, null, null, null ]
-]
+// let board = [
+//     [ null, null, null, null, null, null, null ],
+//     [ null, null, null, null, null, null, null ],
+//     [ null, null, null, null, null, null, null ],
+//     [ null, null, null, null, null, null, null ],
+//     [ null, null, null, null, null, null, null ],
+//     [ null, null, null, null, null, null, null ]
+// ]
 
+function createGameboard(){
+    board = [];
+    for(let i=0; i<6; i++){
+      board[i] = [];
+      for(let j=0; j<7; j++){
+        board[i].push(null);
+      }
+    }
+  }
+console.log[(createGameboard())]
+// This function will create a disk based on the currentPlayer variable setting.
+function createDisk() {
+    if(currentPlayer === 'red') {
+    let diskA = document.createElement('div');
+    diskA.classList.add('diskA');
+    } else {
+        let diskB = document.createElement('div');
+        diskB.classList.add('diskB');
+    }
+}
+createDisk();
+
+// This function will add disk to column using data-col from html.
 function addDiskToBoard (color, boardToUpdate, selectedColumn) {
-
+    // let square = document.getElementById('0-0');
+    // square.appendChild(diskA);
     return updatedBoard
 }
 
@@ -22,6 +46,7 @@ function displayBoardInHTML (boardToDisplay) {
 
 function checkForEndingCondition (boardToCheck) {
     // ending conditions: "red win", "black win", "tie", "" (keep playing)
+    
     return condition
 }
 
@@ -30,6 +55,7 @@ function showMessage () {
 }
 
 function togglePlayer (color) {
+    let newColor
     return newColor
 }
 
