7daf80763d2bae4f56701d68403accb2be2a9b1d
Brandi Cotton <bdcotton52@gmail.com>
2019-09-20 21:59:52 -0500

    Merged changes from Churney repo

--- script.js
+++ script.js
@@ -198,3 +198,111 @@ function togglePlayer (color) {
 //     Check whether the last disc added completed a four-in-a-row within the column (vertically).
 //     Check whether the last disc added completed four-in-a-row horizontally.
 //     Check whether the last disc added completed four-in-a-row on either an upward- or downward-sloping diagonal. -->
+
+let currentPlayer = "red" // "red" "black"
+let board = createGameboard();
+
+//this event listener is global so it runs only once 
+let htmlBoard = document.getElementById("gameboard");
+htmlBoard.addEventListener("click", columnClickHandler);
+
+function columnClickHandler(evt) {
+    // figure out what column was clicked
+    let clickedColumn = evt.target.dataset.col
+    if (clickedColumn === undefined) {
+        return
+    }
+
+    addDiskToBoard(clickedColumn)
+    displayBoardInHTML(board)
+    let condition = checkForEndingCondition(board)
+        // "red win", "black win", "tie", ""
+    if (condition !== "") {
+        showMessage(condition)
+    } else {
+        currentPlayer = togglePlayer(currentPlayer)
+    }
+}
+
+// "red" "black" null
+// let board = [
+//  [null, null, null, null, null, null, null],
+//  [null, null, null, null, null, null, null],
+//  [null, null, null, null, null, null, null],
+//  [null, null, null, null, null, null, null],
+//  [null, null, null, null, null, null, null],
+//  [null, null, null, null, null, null, null]
+// ]
+
+function createGameboard() {
+    let board = [];
+    for (let i = 0; i < 6; i++) {
+        board[i] = [];
+        for (let j = 0; j < 7; j++) {
+            board[i].push(null);
+        }
+    }
+    return board
+}
+
+function createDisk() {
+    let newDisk = document.createElement("div")
+    if (currentPlayer === 'red') {
+        newDisk.className = "diskA"
+    } else {
+        newDisk.className = "diskB"
+    }
+    return newDisk
+}
+
+function addDiskToBoard(clickedColumn) {
+    let newDisk = createDisk();
+    let cells = document.querySelectorAll('[data-col]');
+    columnArray = []
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].getAttribute("data-col") === clickedColumn) {
+            columnArray.push(cells[i])
+        }
+    }
+    columnArray.reverse()
+    for (i = columnArray.length - 1; i >= 0; i--) {
+        if (columnArray[i].childElementCount === 0) {
+            columnArray[i].appendChild(newDisk)
+            if (newDisk.className == 'diskA') {
+                board[i][clickedColumn] = 1;
+            } else {
+                board[i][clickedColumn] = 2;
+            }
+        }
+
+    }
+
+}
+
+function displayBoardInHTML(boardToDisplay) {
+
+    // nothing to return
+}
+
+function checkForEndingCondition(boardToCheck) {
+    // ending conditions: "red win", "black win", "tie", "" (keep playing)
+
+    return condition
+}
+
+function showMessage() {
+    // Tell the user if someone has won or there is a tie
+}
+
+function togglePlayer(color) {
+    let newColor
+    return newColor
+}
+
+// Set a click handler function for each column that adds an additional disc.
+//     Take turns! Toggle the color of each successive disc added.
+//     Keep track of what color disc is at each position in the board. You should be able to console.log() debugging output after each move showing the state of the board.
+//     Once a column is full (has 6 discs), don't allow any more discs to be added.
+//     Check whether the last disc added completed a four-in-a-row within the column (vertically).
+//     Check whether the last disc added completed four-in-a-row horizontally.
+//     Check whether the last disc added completed four-in-a-row on either an upward- or downward-sloping diagonal. -->
